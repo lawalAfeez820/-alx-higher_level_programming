ba7ba47a3eeabe0f17147bb7dd885e3de46926ea
lawalafeez820 <lawalafeez820@gmail.com>
2022-12-01 06:02:53 -0800

    importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     from sys import argv
+    lenOfArg = len(argv)
 
-    length = len(argv) - 1
-
-    if length == 0:
-          print("0 arguments.")
+    if lenOfArg == 1:
+        print("0 arguments.")
+    elif lenOfArg == 2:
+        print("{} argument:".format(lenOfArg - 1))
     else:
-        print(f"{length} arguments:")
-        argv = argv[1:]
-        for i,j in enumerate(argv):
-            print(f"{i + 1}: {j}")
+        print("{} arguments:".format(lenOfArg - 1))
+    for index, var in enumerate(argv):
+        if index == 0:
+            continue
+        print("{}: {}".format(index, var))
