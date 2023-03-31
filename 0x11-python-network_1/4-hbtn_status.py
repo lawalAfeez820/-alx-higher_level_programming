d0c0128d3fb2e30ba21ea1524cf4c40db689b84c
lawalafeez820 <lawalafeez820@gmail.com>
2023-03-31 19:52:43 +0100

    status with request lib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,10 +2,11 @@
 """
 Python script that fetches an URL with requests package
 """
-import requests
+
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    t = r.text
+    import requests
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    t = response.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
