bdd569535a322f927bc753b82d6a06331eb8151a
lawalafeez820 <lawalafeez820@gmail.com>
2023-03-31 19:41:32 +0100

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+- uses urlib.resquest package
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resquest:
-        page_info= resquest.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as request:
+        data_file = request.read()
         print("Body response:")
-        print("\t- type: {}".format(type(page_info)))
-        print("\t- content: {}".format(page_info))
-        print("\t- utf8 content: {}".format(page_info.decode('utf-8')))
+        print("\t- type: {}".format(type(data_file)))
+        print("\t- content: {}".format(data_file))
+        print("\t- utf8 content: {}".format(data_file.decode('utf-8')))
