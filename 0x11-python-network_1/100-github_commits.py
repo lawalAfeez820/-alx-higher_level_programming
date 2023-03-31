da450a5b4b09b6e8ac4d89aee7b201e293e0cd5d
lawalafeez820 <lawalafeez820@gmail.com>
2023-03-31 20:08:37 +0100

    github commit script

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,15 +6,15 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
+    path = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    res = requests.get(path)
+    commits = res.json()
     try:
-        for i in range(10):
+        for num in range(10):
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                commits[num].get("sha"),
+                commits[num].get("commit").get("author").get("name")))
     except IndexError:
         pass
